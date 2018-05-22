ee26232969b34afcf116ecb8fbccda8c70c0ad32
katerinabc <kbohlec@gmail.com>
2018-05-22 12:00:43 +0200

    add r file

---
+++ developer_sna.R
@@ -0,0 +1,43 @@
+# Script for Collaboration Analysis
+# Research Owner: Mahdi
+# latest update: 21st May 2018
+# 
+# 
+
+#clean up
+rm(list=ls())
+# Libraries ---------------------------------------------------------------
+library(sna)
+library(network)
+library(statnet)
+library(foreign)
+library(ergm)
+library(ergm.userterms)
+library(ggplot2)
+
+# data --------------------------------------------------------------------
+
+
+
+# Network description -----------------------------------------------------
+
+#size, degreem nbr networks, 
+#
+
+
+# Task 1: replicate study 1------------------------------------------------------------------
+# replicate Sosa (2004) study @ network level 
+
+# log-linear p1 model --> why ?
+# 
+
+
+# Task 2 ------------------------------------------------------------------
+# logit p* --> do ergm directly
+# 
+# hypothesized effects: 
+
+# Task 3 ------------------------------------------------------------------
+# longitudinal
+
+
