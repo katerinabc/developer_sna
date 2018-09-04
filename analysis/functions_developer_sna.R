41a0306966e7ea53e1d650a062446af5ae57d30d
katerinabc <kbohlec@gmail.com>
2018-09-04 11:36:06 +0100

    commenting on code

--- analysis/functions_developer_sna.R
+++ analysis/functions_developer_sna.R
@@ -50,3 +50,46 @@ return(list(A, Arow))
 
 }
 
+
+# vlookup -----------------------------------------------------------------
+
+#Vlookup in R
+#Version 0.3 November 12, 2013
+#Return senesical results if return column is a factor
+#Version 0.2 November 11, 2013
+#Require first column of table to be numeric if range lookup is being done
+#Change defaults to larger=FALSE
+#Julin Maloof
+
+# https://gist.github.com/jnmaloof/7367450
+# 
+vlookup <- function(ref, #the value or values that you want to look for
+                    table, #the table where you want to look for it; will look in first column
+                    column, #the column that you want the return data to come from,
+                    range=FALSE, #if there is not an exact match, return the closest?
+                    larger=FALSE) #if doing a range lookup, should the smaller or larger key be used?)
+{
+  if(!is.numeric(column) & !column %in% colnames(table)) {
+    stop(paste("can't find column",column,"in table"))
+  }
+  if(range) {
+    if(!is.numeric(table[,1])) {
+      stop(paste("The first column of table must be numeric when using range lookup"))
+    }
+    table <- table[order(table[,1]),] 
+    index <- findInterval(ref,table[,1])
+    if(larger) {
+      index <- ifelse(ref %in% table[,1],index,index+1)
+    }
+    output <- table[index,column]
+    output[!index <= dim(table)[1]] <- NA
+    
+  } else {
+    output <- table[match(ref,table[,1]),column]
+    output[!ref %in% table[,1]] <- NA #not needed?
+  }
+  dim(output) <- dim(ref)
+  output
+}
+
+
