79e635425781370c68e46fa63015041a28662310
katerinabc <kbohlec@gmail.com>
2018-09-25 21:43:21 +0100

    finished checking owernship file

--- analysis/ownership_developer_sna.R
+++ analysis/ownership_developer_sna.R
@@ -199,16 +199,6 @@ View(DF[DF$members == FALSE, c(43:45)]) # visual inspection of folder owner chan
 table(DF[DF$members == FALSE, 43] == DF[DF$members == FALSE, 45])
 # none true. 
 
- 
-# block of code to find out errors in ownership change. error was due to coding of versions in DF
-# View(DF[(DF$members == FALSE) & (DF$folder_owner == DF$new_owner),c(1,3, 6:7, 43:45)])
-# # paride.ciatto is in version 6 - 13 --> he must be in version 5. he is listd there several times
-# # longbow.liu is in versino 7 - 13
-# 
-# View(DF[DF$author == "paride.ciatto", c(1,3, 6:7)])
-# View(DF[DF$author == "longbow.liu", c(1,3, 6:7)])
-
-
 ownership_change <- reshape2::melt(table(DF$folder_owner, DF$new_owner))
 ownership_change <- ownership_change[-which(as.character(ownership_change$Var1) == 
                                               as.character(ownership_change$Var2)),]
@@ -232,52 +222,20 @@ ggsave('ownership_change.png')
 # earlier information: IDs are unique per version. 
 
 # goal: in task_v2 replace the file_id with the owner's name
-# workflow: 
-task_v2 <- DF2[DF2$file == "1.5",] #file 1.5 is version 2 according to the new version coding 
+# 
+#file 1.5 is version 2 according to the new version coding 
 # schema - Mahdi 7/9/18
+task_v2 <- DF2[DF2$file == "1.5",] 
 
 # this is the lookup table, containing the ID (col 7) and the folder owner name (col 45)
 ownership_withid <- DF[, c(7, 45)] 
 ownership_withid <- ownership_withid[order(ownership_withid$ID_File_und),]
-
-# delete this comment?
-# It needs to be the complete dataset (DF) for when a file hasn't been modified in this
-# version. if the file hasn't been modified in ver4, it will not be shown in 
-# dev4_raw. This will result in NA for when matching needed and required 
-# communication
  
 # match the IDs in reqcomm_v2 with the IDs in the lookup table ownership_withid. 
 # match returns a numerical vector. The number indicates the position (row number)
 # when the first match happened. In other words it checks in which row an ID mentioned 
 # in reqcomm_v2 appears in ownership_withid$ID_FILE_und. This row number is used to 
 # assign the correct owner from the vector ownership_withinid$new_owner
- 
-# doesn't work as expected
-# library(data.table)
-# DT1 <- as.data.table(DF2[DF2$file == "1.5",1]) # sender
-# DT2 <- as.data.table(ownership_withid)
-# 
-# setkey(DT1, V1)
-# setkey(DT2, ID_File_und)
-# sender <- DT1[DT2]
-# 
-# DT1 <- as.data.table(DF2[DF2$file == "1.5",2]) # receiver
-# setkey(DT1, V1)
-# receiver <- DT1[DT2]
-# 
-# sender <- DF2[DF2$file == "1.5",c(1:2)] %>% left_join(ownership_withid, by = c('und_from_file_id'= 'ID_File_und'))
-# receiver <- DF2[DF2$file == "1.5",c(1:2)] %>% left_join(ownership_withid, by = c('und_to_file_id'= 'ID_File_und'))
-# 
-
-# #STOPPED HERE WITH REVIEWNG CODE. 
-# reqcomm_v2 <- task_v2[,c(1:2)] # subset task4 to the technical dependencies
-# reqcomm_v2[] <- ownership_withid$new_owner[match(
-#   unlist(reqcomm_v2), ownership_withid$ID_File_und)]
-# 
-# reqcomm_v2$weight <- task_v2[,3] # add the weights of the technical dependencies
-# 
-# # check how many files were not matched. 
-# table(is.na(reqcomm_v2))
 
 # This might be a bit weird, but I'm going to do mini steps to make sure it workds. 
 # Get a vector of sender and receivers. this vector contains the file ID
@@ -288,10 +246,8 @@ receiver_id <- task_v2[,2]
 sender_name <- ownership_withid$new_owner[match(sender_id, ownership_withid$ID_File_und)]
 receiver_name <- ownership_withid$new_owner[match(receiver_id, ownership_withid$ID_File_und)]
 
-# 29025 ids are not matched. Replace NA's with the id number
-
-# the files which don't have a folder owner name (no match) are NA for the moment. Replace NA with the
-# original file ID number.
+# the files which don't have a folder owner name (no match) are NA for the moment. 
+# Replace NA with the original file ID number for testing purposes
 snd_na_idx <- which(is.na(sender_name))
 sender_name[snd_na_idx] <- sender_id[snd_na_idx]
 
@@ -300,40 +256,29 @@ receiver_name[rcv_na_idx] <- receiver_id[rcv_na_idx]
 
 # create the micro task communication edgelist
 reqcomm_v2 <- cbind(sender_name, receiver_name, task_v2$weight)
-# View(reqcomm_v2)
+View(reqcomm_v2)
 
 # count number of file IDs that have no owner
 length(snd_na_idx) + length(rcv_na_idx) # 27678
 
 # files with no assigned folder owner
-no_folder_owner <- as.data.frame(c(sender_id[snd_na_idx], receiver_id[rcv_na_idx]))
-length(unique(no_folder_owner$x)) # 2657 files --> need artificial name as this will be a big network
+no_folder_owner <- c(sender_id[snd_na_idx],receiver_id[rcv_na_idx])
+length(unique(no_folder_owner)) # 2657 files --> need artificial name as this will be a big network
 
-
+tail(sort(table(no_folder_owner)))
 # Ideal workflow: find out number of files with no owner. decide if keep file ID as artifical
 # folder owner name or replace with artifical name. 
-no_folder_owner %>% count()
 ggplot(as.data.frame(no_folder_owner), aes(x = no_folder_owner)) + geom_bar()
 
+# the id's that are not matched do not appear in the dataset DF
+# test assumption with id 43748, 36927, 15330
+DF[DF$ID_File_und == 15330, c(1:4, 41, 45)]
+DF[DF$ID_File_und == 43748, c(1:4, 41, 45)]
+DF[DF$ID_File_und == 36927, c(1:4, 41, 45)]
 
-
-# OLD EXPLANATION. STILL VALID ??
-# the files in reqcomm_v2 who have NA have not been modified in version 4. as the Id's are 
-# unique per version it is not possible to indicate to what folder this file belongs.
-# therefore it is not possible to assign ownership. 
-# a fictious developer ('notmodified') is assigned to these files
-
-# NEW EXPLANATION: 
-# The IDs that have not been matched with a developer name have not been modified in 
-# ver 4, but might have been in a previous version. check the previous version etc
-# remember the splitting of software !!!
-
-idx <- which(is.na(reqcomm_v2$und_from_file_id))
-reqcomm_v2[idx,1] <- 'not.modified'
-
-idx <- which(is.na(reqcomm_v2$und_to_file_id))
-reqcomm_v2[idx,2] <- 'not.modified'
-
+# turn the id's for which no owner was identified into 'external_owner'
+reqcomm_v2[snd_na_idx,1] <- 'external_owner'
+reqcomm_v2[rcv_na_idx,2] <- 'external_owner'
 
 # remove all temporrary files
 rm(list=ls()[grep('tmp', ls())])
